6569209d30514b51061ab002d48dbfa2bb9766f4
VanesaGarnica <60490756+VanesaGarnica@users.noreply.github.com>
2020-12-02 23:34:03 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Bienvenidos al Coder-Commerce!</h1>
+    <p>podrás hacer todas tus compras sin moverte de tu casa</p>
+    </>
   );
 }
 
